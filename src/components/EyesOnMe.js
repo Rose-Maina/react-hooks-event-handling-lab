0f04ff850fba7c382f9cceb460f6839d7e513b0b
Rose Maina <rosewairimu27@gmail.com>
2022-07-14 10:13:02 +0300

    Done.

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,19 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+    function clickFocus() {
+        console.log("Good!");
+    }
+
+    function clickBlur() {
+        console.log("Hey! Eyes on me!");
+    }
+
+    return (
+        <button onFocus={clickFocus} onBlur={clickBlur}>Eyes on me</button>
+    );
+    }
+
+export default EyesOnMe;
+    
